5d4c018d679907a53737520c70c85162dda0a4d4
ahmed toba <ahmedtobashahban21@gmail.com>
2024-08-04 19:57:21 +0300

    first commit

---
+++ ContactPageForTesting/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using ContactPageForTesting.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ContactPageForTesting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ContactController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost("send")]
+        public async Task<IActionResult> SendEmailAsync([FromBody] EmailMessage message)
+        {
+            try
+            {
+                var emailMessage = new MimeMessage();
+                emailMessage.From.Add(new MailboxAddress("Ahmed Toba", _configuration["SmtpSettings:Username"]));
+                emailMessage.To.Add(new MailboxAddress("", message.To));
+                emailMessage.Subject = message.Subject;
+                emailMessage.Body = new TextPart("plain")
+                {
+                    Text = message.Body
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync("smtp.gmail.com", 587, false);
+                    await client.AuthenticateAsync(_configuration["SmtpSettings:Username"], _configuration["SmtpSettings:Password"]);
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Failed to send email: {ex.Message}");
+            }
+        }
+    }
+
+}
+
